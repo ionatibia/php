7e49e7cea2e07632a206c3304cc8f70919521c0e
natiexperiencia <natiexperiencia@gmail.com>
2015-09-11 11:18:27 +0200

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # php
 Pruebas y ejercicios con PHP
+
+#show errors on web browser
+discoment E_ALL on php.ini (show all errors)
---
+++ prueba/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	</head>
+	<body>
+		<?php
+			$izena = "Iker";
+			$abizena = "Oñatibia";
+			$izenAbizena = $izena.' '.$abizena
+			echo "Mi nombre es: $izena y tengo 33 años (como Jesucristo)\n<br/>";
+			echo 'Mi nombre es: ' .$izena.' y tengo 33 años (como Jesucristo)';
+			echo "<br/>$izenAbizena";
+		?>
+</body>
